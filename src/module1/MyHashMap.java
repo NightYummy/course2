7b677ee20eb54d2a00153de901a896bdf161ae10
NightYummy <alexkol200127@gmail.com>
2026-02-26 03:52:42 +0500

    added MyHashMap class

---
+++ src/module1/MyHashMap.java
@@ -0,0 +1,73 @@
+package module1;
+
+class MyHashMap<K, V> {
+
+    private final Pair<K, V>[] data = new Pair[16];
+
+    public V get(K key) {
+        int index = getIndex(key);
+        var pair = data[index];
+
+        if (pair == null) return null;
+
+        while (!pair.key.equals(key)) pair = pair.next;
+
+        return pair.value;
+    }
+
+    public V put(K key, V value) {
+        int index = getIndex(key);
+        var pair = data[index];
+
+        if (data[index] == null) {
+            data[index] = new Pair<>(key, value);
+            return data[index].value;
+        }
+
+        while (pair.next != null) pair = pair.next;
+
+        pair.next = new Pair<>(key, value);
+        pair.next.previous = pair;
+
+        return pair.value;
+    }
+
+    public V remove(K key) {
+        int index = getIndex(key);
+        var pair = data[index];
+
+        if (pair == null) return null;
+
+        if (pair.key.equals(key)) {
+            data[index] = pair.next;
+            pair.next = null;
+            return pair.value;
+        }
+
+        while (!pair.key.equals(key)) pair = pair.next;
+
+        pair.previous.next = pair.next;
+        if (pair.next == null) return pair.value;
+        pair.next.previous = pair.previous;
+
+        return pair.value;
+    }
+
+    private int getIndex(K key) {
+        if (key == null) return 0;
+        return Math.abs(key.hashCode() % data.length);
+    }
+
+    private static class Pair<K, V> {
+
+        K key;
+        V value;
+        Pair<K, V> next;
+        Pair<K, V> previous;
+
+        Pair(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+}
